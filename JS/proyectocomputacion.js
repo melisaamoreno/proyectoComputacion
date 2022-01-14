dcc124238f6dfe626b2caef4f9078f02e0322063
melisa <melisaamoreno@gmail.com>
2022-01-14 01:16:49 -0300

    edite selects desde JS

--- JS/proyectocomputacion.js
+++ JS/proyectocomputacion.js
@@ -1,9 +1,11 @@
-//Agregar venta //
+//  ---------- AGREGAR VENTA ---------- //
 const btnModal = document.querySelector('#btnVenta');
 const btnClose = document.querySelector('.close');
 const btnCancel = document.querySelector('#cancelar');
 const modal = document.querySelector('.modal-container');
 
+
+//            EVENTOS           // 
 btnModal.addEventListener('click', () => {
     modal.style.display = "block"
 })
@@ -12,7 +14,7 @@ btnClose.addEventListener('click', () => {
     modal.style.display = "none"
 })
 
-// Editar venta //  ** falta btn de guardar el edit **
+//  ----------- EDITAR VENTA ----------- //
 const btnEditar = document.querySelector('.pen')
 const editarModal = document.querySelector('.edit')
 const closebtn = document.querySelector('#xcloser')
@@ -25,7 +27,7 @@ closebtn.addEventListener('click', () => {
 })
 
 
-// Eliminar venta // **falta btn de eliminar **
+// ---------- ELIMINAR VENTA ----------- // 
 const closer = document.querySelector('#xclose');
 const modalDelete = document.querySelector('.modalDelete');
 const trash = document.querySelector('.trash');
@@ -52,48 +54,176 @@ const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
 const componentes = ["Monitor GPRS 3000","Motherboard ASUS 1500", "Monitor ASC 543","Motherboard ASUS 1200", "Motherboard MZI", "HDD Toyiva", "HDD Wezter Dishital", "RAM Quinston", "RAM Quinston Fury"]
 
 
-const ventas = [[1, new Date(2019, 1, 4), "Grace", "Centro", ["Monitor GPRS 3000", "Motherboard ASUS 1500"]], [2, new Date(2019, 0, 1), "Ada", "Centro", ["Monitor GPRS 3000", "Motherboard ASUS 1500"]], [3, new Date(2019, 0, 2), "Grace", "Caballito", ["Monitor ASC 543", "Motherboard MZI"]], [4, new Date(2019, 0, 10), "Ada", "Centro", ["Monitor ASC 543", "Motherboard ASUS 1200"]], [5, new Date(2019, 0, 12).toLocaleDateString(), "Grace", "Caballito", ["Monitor GPRS 3000", "Motherboard ASUS 1200"]]];
+const ventas = [
+  [
+    1,
+   new Date(2019, 1, 4),
+   "Grace",
+   "Centro",
+   ["Monitor GPRS 3000", "Motherboard ASUS 1500"]
+  ],
+  [
+    2, 
+   new Date(2019, 0, 1), 
+   "Ada", 
+   "Centro", 
+   ["Monitor GPRS 3000", "Motherboard ASUS 1500"]
+  ], 
+  [
+    3, 
+    new Date(2019, 0, 2), 
+    "Grace", 
+    "Caballito", 
+    ["Monitor ASC 543", "Motherboard MZI"]
+  ], 
+  [
+    4, 
+    new Date(2019, 0, 10), 
+    "Ada", 
+    "Centro", 
+    ["Monitor ASC 543", "Motherboard ASUS 1200"]
+  ], 
+  [
+    5, 
+    new Date(2019, 0, 12), 
+    "Grace", 
+    "Caballito", 
+    ["Monitor GPRS 3000", "Motherboard ASUS 1200"]
+]]
 
 
-const precios = [["Monitor GPRS 3000", 200], ["Motherboard ASUS 1500", 120], ["Monitor ASC 543", 250], ["Motherboard ASUS 1200", 100],["Motherboard MZI", 30], ["HDD Toyiva", 90], ["HDD Wezter Dishital", 75], ["RAM Quinston", 110], ["RAM Quinston Fury", 230]];
+const precios = [
+["Monitor GPRS 3000", 200], 
+["Motherboard ASUS 1500", 120], 
+["Monitor ASC 543", 250], 
+["Motherboard ASUS 1200", 100],
+["Motherboard MZI", 30], 
+["HDD Toyiva", 90], 
+["HDD Wezter Dishital", 75], 
+["RAM Quinston", 110], 
+["RAM Quinston Fury", 230]
+];
+
+const sucursales = ["Centro", "Caballito"];
+
+// ---------------------------------------------------------- // 
+
+//  IMPRIMIR EL ARRAY EN LA TABLA //
+
+const format = (date, locale, options) => new Intl.DateTimeFormat(locale, options).format(date);
+
+const tabla = document.getElementById('tables')
+const tablaVentas = () => {
 
 
-function tablaVentas (array){
-const tabla = document.querySelector('#ventas')
-const tr = document.createElement('tr')
+  for (let i = 0; i < ventas.length; i++){
+    const newTr = document.createElement('tr');
+    tabla.appendChild(newTr)
+
+    for (let j = 0; j < ventas[i].length; j++) {
+
+      const fechaVentas = format(ventas[i][1] ,'es')
+      console.log (fechaVentas)
+      const nombreVenta = ventas[i][2]
+      console.log(nombreVenta)
+      const sucursalVenta = ventas[i][3]
+      console.log(sucursalVenta)
+      const componentesVenta = ventas[i][4]
+      console.log(componentesVenta)
+    
+       
+       newTr.innerHTML = `<td>${fechaVentas},</td><td>${nombreVenta}</td><td>${sucursalVenta}</td><td>${componentesVenta}</td> <td>precio</td>`
+    
+    
+      } }
+
+  }
 
 
-  for (let i = 0; i < array.length; i++) {
-  const fechaVentas = array[i][1]
-  //console.log (fechaVentas)
-  const nombreVenta = array[i][2]
-  //console.log(nombreVenta)
-  const sucursalVenta = array[i][3]
-  //console.log(sucursalVenta)
-  const componentesVenta = array[i][4]
-  //console.log(componentesVenta)
+tablaVentas()
+// ------------------------------------------------------------ // 
+
+
+//      AGREGAR SELECTS  // AGREGAR VENTA  //
+
+const selectVendedora = document.querySelector('.select-vendedora');
+
+const agregarVendedoras = () => {
+  for (let i = 0; i < vendedoras.length; i++){
+    const selectVend = document.createElement('option')
+    /*selectVend.classList.add('selects')*/
+    selectVendedora.appendChild(selectVend)
+    selectVend.innerHTML = `${vendedoras[i]}`
+  }
+}
+
+agregarVendedoras()
+
+const selectComponentes = document.querySelector('.multiple');
+
+const agregarComponentes = () => {
+  for (let i = 0; i < precios.length; i++){
+    const compo = document.createElement('option')
+    selectComponentes.appendChild(compo)
+    compo.innerHTML = `${precios[i][0]}`
   
-
- tr.innerHTML = `<td>${fechaVentas}</td><td>${nombreVenta}</td><td>${sucursalVenta}</td><td>${componentesVenta}</td>`
- 
- tabla.appendChild(tr)
- }
- 
+  }
 }
 
-console.log(tablaVentas(ventas))
+agregarComponentes()
 
+const selectSucursal = document.querySelector('.sede');
 
-
-
-
-
-
-
-
-//obtener valor de un Select //
-function selectValue (){
-  let selectedValue = document.querySelector('componentes').value;
-
-  console.log(selectedValue)
+const agregarSucursal = () => {
+  for (let i = 0; i < sucursales.length; i++){
+    const sucursal = document.createElement('option')
+    selectSucursal.appendChild(sucursal)
+    sucursal.innerHTML = `${sucursales[i]}`
+  
+  }
 }
+
+agregarSucursal()
+
+
+//       AGREGAR SELECTS  //  EDITAR  VENTA   ///
+
+const selectSeller = document.querySelector('.select-seller');
+
+const addVendedoras = () => {
+  for (let i = 0; i < vendedoras.length; i++){
+    const sellers = document.createElement('option')
+    selectSeller.appendChild(sellers)
+    sellers.innerHTML = `${vendedoras[i]}`
+  }
+}
+ 
+addVendedoras()
+
+/*const selectMultiple = document.querySelector('.selectmultiple');
+
+const multiSelect = () => {
+  for (let i = 0; i < precios.length; i++)
+  const options = document.createElement('option')
+  selectMultiple.appendChild(options)
+  options.innerHTML = `${precios[i][0]}`
+}
+
+multiSelect()*/
+
+
+
+
+
+// PRECIO
+
+/*function precioMaquina (componentes) {
+  for (let i= 0; i < array.length; i++) {
+    console.log(array)
+    return array
+  }
+  if (array.includes(componentesVenta)){
+    
+  }
+}
+precioMaquina()*/
